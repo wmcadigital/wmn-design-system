a01ef6f13f99a17c55fd3c26bebc3dba446d4955
Design Team 1 <macuser@macs-air.centronet.centro.org.uk>
2019-10-02 12:37:14 +0100

    button components updated to use handlebars

--- src/views/styleguide/styleguide.js
+++ src/views/styleguide/styleguide.js
@@ -1,94 +1,21 @@
-/* eslint-disable no-loop-func */
-// Define DOM elements
-const iframe = document.getElementById('resizer');
-// const logo = document.getElementById('header-logo');
-const links = document.getElementsByClassName('side-menu__sub-menu__item-link');
-
-// Define url vars
-let currentUrl;
-const pagesDir = 'pages/';
-const pages = {
-    Home: {
-        title: 'home',
-        link: `${pagesDir}/home.html`,
-    },
-    ColorPalette: {
-        title: 'colorPalette',
-        link: `${pagesDir}/colorPalette.html`,
-    },
-    Typography: {
-        title: 'typography',
-        link: `${pagesDir}/typography.html`,
-    },
-    Marks: {
-        title: 'marks',
-        link: `${pagesDir}/marks.html`,
-    },
-    Tables: {
-        title: 'tables',
-        link: `${pagesDir}/tables.html`,
-    },
-    Buttons: {
-        title: 'buttons',
-        link: `${pagesDir}/buttons.html`,
-    },
-    Header: {
-        title: 'header',
-        link: `${pagesDir}/header.html`,
-    },
-    FormElements: {
-        title: 'formElements',
-        link: `${pagesDir}/formElements.html`,
-    },
-};
-
-function LoadPage(href) {
-    currentUrl = href;
-    switch (href) {
-    case pages.Typography.title:
-        iframe.setAttribute('src', pages.Typography.link);
-        break;
-    case pages.ColorPalette.title:
-        iframe.setAttribute('src', pages.ColorPalette.link);
-        break;
-    case pages.Marks.title:
-        iframe.setAttribute('src', pages.Marks.link);
-        break;
-    case pages.Tables.title:
-        iframe.setAttribute('src', pages.Tables.link);
-        break;
-    case pages.Buttons.title:
-        iframe.setAttribute('src', pages.Buttons.link);
-        break;
-    case pages.Header.title:
-        iframe.setAttribute('src', pages.Header.link);
-        break;
-    case pages.FormElements.title:
-        iframe.setAttribute('src', pages.FormElements.link);
-        break;
-    default:
-        iframe.setAttribute('src', pages.Home.link);
-        break;
+function pageRouting() {
+    // Define DOM elements
+    const iframe = document.getElementById('resizer');
+    const links = document.querySelectorAll('.side-menu__sub-menu__item-link'); // Get all sidebar links
+    // Define url vars
+    const pagesDir = 'pages/';
+    // Foreach link in links
+    links.forEach((link) => {
+        const href = `${pagesDir + link.getAttribute('href').replace('#', '')}.html`; // Define the actual href of the page
+        // On link click, inject the above href into iframe
+        link.addEventListener('click', () => {
+            iframe.setAttribute('src', href);
+        });
+    });
+    // If the URL contains a hash element
+    if (window.location.hash) {
+        const href = `${pagesDir + window.location.hash.replace('#', '')}.html`; // Define the actual href of the page
+        iframe.setAttribute('src', href); // inject the above href into iframe
     }
 }
-
-for (let link = 0; link < links.length; link += 1) {
-    links[link].addEventListener('click', () => {
-        // get the anchor element that wraps around the link
-        const parent = links[link].parentElement.parentElement;
-        let href;
-
-        // check if parent element exists and if it does get the
-        // value of the href attribute and remove any hashtags
-        if (parent) {
-            href = parent.getAttribute('href').replace('#', '');
-        }
-
-        // Check if link matches the current page
-        if (currentUrl !== href) {
-            LoadPage(href);
-        }
-    });
-}
-
-// LoadPage(window)
+window.addEventListener('DOMContentLoaded', pageRouting); // call pageRouting as soon as DOM is loaded (need to wait for DOM due to iframe injection)
