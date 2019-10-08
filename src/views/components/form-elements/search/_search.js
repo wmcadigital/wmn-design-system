5e6f3274e1d95bbbb757bfce50b1c52092ac2227
Dayle <dayle.salmon@wmca.org.uk>
2019-10-08 14:18:13 +0100

    Started on autocomplete functionality

--- src/views/components/form-elements/search/_search.js
+++ src/views/components/form-elements/search/_search.js
@@ -1,51 +1,50 @@
-window.addEventListener('load', () => {
-    // Add a keyup event listener to our input element
-    const name_input = document.getElementById('myInput');
-    name_input.addEventListener('keyup', (event) => { hinter(event); });
-
-    // create one global XHR object
-    // so we can abort old requests when a new one is make
-    window.hinterXHR = new XMLHttpRequest();
-});
-
-// Autocomplete for form
-function hinter(event) {
-
-    // retireve the input element
-    const input = event.target;
-
-    // retrieve the datalist element
-    const huge_list = document.getElementById('huge_list');
-
-    // minimum number of characters before we start to generate suggestions
-    const min_characters = 0;
-
-    if (input.value.length < min_characters) {
-        return;
+const autoCompleteEle = document.querySelector("#autoComplete");
+new autoComplete({
+    data: {                              // Data src [Array, Function, Async] | (REQUIRED)
+      src: async () => {
+        // User search query
+        autoCompleteEle.value;
+        // Fetch External Data Source
+        const source = await fetch(`https://jsonplaceholder.typicode.com/users`);
+        // Format data into JSON
+        const data = await source.json();
+        // Return Fetched data
+        console.log(data);
+        return data;
+      },
+      key: ["name"],
+      cache: true
+    },
+    sort: (a, b) => {                    // Sort rendered results ascendingly | (Optional)
+        if (a.match < b.match) return -1;
+        if (a.match > b.match) return 1;
+        return 0;
+    },
+    placeHolder: "Tap to search",     // Place Holder text                 | (Optional)
+    selector: "#autoComplete",           // Input field selector              | (Optional)
+    threshold: 3,                        // Min. Chars length to start Engine | (Optional)
+    debounce: 300,                       // Post duration for engine to start | (Optional)
+    searchEngine: "strict",              // Search Engine type/mode           | (Optional)
+    resultsList: {                       // Rendered results list object      | (Optional)
+        render: true,
+        container: source => {
+            const resultsListID = "results_list";
+            return resultsListID;
+        },
+        destination: document.querySelector("#autoComplete"),
+        position: "afterend",
+        element: "ul"
+    },
+    maxResults: 5,                         // Max. number of rendered results | (Optional)
+    highlight: true,                       // Highlight matching results      | (Optional)
+    resultItem: {                          // Rendered result item            | (Optional)
+        content: (data, source) => {
+            source.innerHTML = data.match;
+        },
+        element: "li"
+    },
+    onSelection: feedback => {             // Action script onSelection event | (Optional)
+        autoCompleteEle.value = feedback.selection.value.name;
+        console.log(feedback);
     }
-
-    // abort any pending requests
-    window.hinterXHR.abort();
-
-    window.hinterXHR.onreadystatechange = () => {
-        if (this.readyState === 4 && this.status === 200) {
-            // We're expecting a json response so we convert it to an object
-            const response = JSON.parse(this.responseText);
-
-            // clear any previously loaded options in the datalist
-            huge_list.innerHTML = '';
-
-            response.forEach((item) => {
-                // Create a new <option> element.
-                const option = document.createElement('option');
-                option.value = item;
-
-                // attach the option to the datalist element
-                huge_list.appendChild(option);
-            });
-        }
-    };
-
-    window.hinterXHR.open('GET', '//static.centro.org.uk/assets/nwm/feeds/journeyplanner_options_stateless.json', true);
-    window.hinterXHR.send();
-}
+});
