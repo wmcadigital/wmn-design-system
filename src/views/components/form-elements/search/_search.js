c82eca5bb4ad8712b968979e3dc745c3bd649420
Dayle <dayle.salmon@wmca.org.uk>
2019-10-10 15:54:30 +0100

    Autcomplete is now dynamic

--- src/views/components/form-elements/search/_search.js
+++ src/views/components/form-elements/search/_search.js
@@ -1,57 +1,56 @@
-const autoCompleteInputs = document.querySelectorAll(".fe-autocomplete__input");
+// Library from: https://github.com/TarekRaafat/autoComplete.js
+const autoCompleteInputs = document.querySelectorAll(".wmn-fe-autocomplete__input");
 
 autoCompleteInputs.forEach((autoCompleteEle, i) => {
 
-    console.log(autoCompleteEle);
-
-    autoCompleteInputs[i].id = 'js-autoComplete-' + id;
-
-    const ds = autoCompleteEle.dataset;
+    autoCompleteEle.id = 'wmn-js-autoComplete-' + i; // Foreach autoCompleteEle, generate unique id to be used elsewhere
+    const ds = autoCompleteEle.dataset; // Get all datasets from said input
+    // Set configs to use below
     const config = {
-        api: ds.api || null,
-        placeHolder: ds.placeholder || "Tap to search",
+        api: ds.api || null, // Use API string OR null
+        keys: ds.keys.replace(/ /g,'').split(',') || [], // Get rid of white space and turn into array OR default to blank array
+        placeHolder: ds.placeholder || "Tap to search", // Use placeholder string OR "Tap to search" deafult
     };
 
-    new autoComplete({
-        data: { // Data src [Array, Function, Async] | (REQUIRED)
-          src: async () => {
-              if(config.api){
-                  const source = await fetch(config.api); // Fetch External Data Source
-                  const data = await source.json(); // Format data into JSON
-                  return data; // Return Fetched data
-              }else{
-                  return false; // Return nothing (fail silently)
-              }
-          },
-          key: ["name"],
-          cache: true
-        },
-        placeHolder: config.placeHolder,     // Place Holder text                 | (Optional)
-        selector: '#autComplete',           // Input field selector              | (Optional)
-        threshold: 3,                        // Min. Chars length to start Engine | (Optional)
-        debounce: 300,                       // Post duration for engine to start | (Optional)
-        searchEngine: "strict",              // Search Engine type/mode           | (Optional)
-        resultsList: {                       // Rendered results list object      | (Optional)
-            render: true,
-            container: source => {
-                const resultsListID = "results_list";
-                return resultsListID;
+    // If there is a valid api string, then create a new autocomplete instance,
+    // the closest pre, is to stop the code launching in the pre/code tags on the styleguide
+    if (config.api && autoCompleteEle.closest('pre') === null){
+        new autoComplete({
+          // Data src [Array, Function, Async] | (REQUIRED)
+          data: {
+              src: async () => {
+                const source = await fetch(config.api); // Fetch External Data Source
+                const data = await source.json(); // Format data into JSON
+                return data; // Return Fetched data
+              },
+              key: config.keys, // Use keys provided to match search
+              cache: true
             },
-            destination: document.querySelector("#autoComplete"),
-            position: "afterend",
-            element: "ul"
-        },
-        maxResults: 5,                         // Max. number of rendered results | (Optional)
-        highlight: true,                       // Highlight matching results      | (Optional)
-        resultItem: {                          // Rendered result item            | (Optional)
-            content: (data, source) => {
-                source.innerHTML = data.match;
+            placeHolder: config.placeHolder,     // Place Holder text
+            selector: '#' + autoCompleteEle.id,  // Input field selector
+            threshold: 0, // Min. Chars length to start Engine
+            debounce: 300, // Post duration for engine to start
+            resultsList: { // Rendered results list object
+                render: true, // Render out results
+                container: (source) => {
+                    const resultsListID = 'results_list' + i; // Set ID to unique
+                    source.id = resultsListID; // Set above
+                    source.classList.add('wmn-fe-autocomplete__results-list'); // Add correct BEM class to results
+                    return resultsListID;
+                },
+                destination: document.querySelector('#' + autoCompleteEle.id)
             },
-            element: "li"
-        },
-        onSelection: feedback => {             // Action script onSelection event | (Optional)
-            autoCompleteEle.value = feedback.selection.value.name;
-            console.log(feedback);
-        }
-    });
+            highlight: true, // Highlight matching results
+            // Rendered result item
+            resultItem: {
+                content: (data, source) => {
+                    source.classList.add('wmn-fe-autocomplete__result'); // Add correct BEM class to results
+                    source.innerHTML = data.match;
+                }
+            },
+            onSelection: feedback => {// Action script onSelection event
+                autoCompleteEle.value = feedback.selection.value.name;
+            }
+        });
+    }
 });
