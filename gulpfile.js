a66b492d7c6e7dc4e6eef2269ec509463ddac4c5
Dayle <dayle.salmon@wmca.org.uk>
2019-10-24 11:24:30 +0100

    moved lintJS into seperate file

--- gulpfile.js
+++ gulpfile.js
@@ -124,12 +124,7 @@ const buildStyles = require('./gulp-tasks/build-styles');
 const buildScripts = require('./gulp-tasks/build-scripts');
 
 // Lint scripts/JS
-function lintScripts() {
-  return src('src/views/**/*.js')
-    .pipe(eslint({ configFile: '.eslintrc.json' })) // eslint() attaches the lint output to the "eslint" property of the file object so it can be used by other modules.
-    .pipe(eslint.format()); // eslint.format() outputs the lint results to the console.
-  // .pipe(eslint.failAfterError()); // Cause the stream to stop/fail before copying an invalid JS file to the output directory
-}
+const lintScripts = require('./gulp-tasks/lint-scripts');
 
 // Lint Templates/HTML
 function lintTemplates() {
