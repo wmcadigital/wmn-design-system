b816d0cad021bca125369f8f195c9b675f3d24d1
Dayle <dayle.salmon@wmca.org.uk>
2019-10-25 16:25:01 +0100

    Started work on pulling in data.json to nunjucks

--- gulp-tasks/nunjucks.js
+++ gulp-tasks/nunjucks.js
@@ -1,12 +1,25 @@
 // Gulp requires
 const { src, dest } = require('gulp');
 const plugins = require('gulp-load-plugins')();
+const fs = require('fs');
 // Local requires
+const path = require('path');
 const paths = require('./config.js');
 const { packageJson, build } = require('./utils');
 
+function getDataForFile(file) {
+  try {
+    // If filename-data.json exists then use it below for nunjucks file's data
+    return JSON.parse(fs.readFileSync(`${path.parse(file.path).name}-data.json`));
+  } catch (err) {
+    // console.log(err);
+    return false;
+  }
+}
+
 module.exports = () => {
   return src(paths.nunjucks.src)
+    .pipe(plugins.data(getDataForFile))
     .pipe(
       plugins.nunjucksRender({
         path: 'src/views/',
@@ -19,7 +32,7 @@ module.exports = () => {
         extname: '.html'
       })
     )
-    .pipe(plugins.htmlmin({ collapseWhitespace: true }))
+    .pipe(plugins.htmlmin({ collapseWhitespace: true, removeComments: true }))
     .pipe(plugins.formatHtml())
     .pipe(dest(paths.nunjucks.output));
 };
