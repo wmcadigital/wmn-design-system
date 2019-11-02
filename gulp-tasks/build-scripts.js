c56b5b5c588c040f7d14aa132332f1d4b8e33dee
Dayle Salmon <dayle.salmon@wmca.org.uk>
2019-11-02 12:45:58 +0000

    Tidied code in buildscripts

--- gulp-tasks/build-scripts.js
+++ gulp-tasks/build-scripts.js
@@ -1,39 +1,36 @@
-// require('core-js/stable');
-// require('regenerator-runtime/runtime');
 // Gulp requires
 const { src, dest } = require('gulp');
 const plugins = require('gulp-load-plugins')();
 const webpack = require('webpack-stream');
 // Local requires
 const paths = require('./paths.js');
-const { getRoot, packageJson, build } = require('./utils');
+const { packageJson, build } = require('./utils');
+
+const buildMode = build !== 'local' ? 'production' : 'development'; // If the build isn't local then set the buildMode of webpack to production (it will hide sourcemaps and minify code)
 
 // Placeholder function for buildScripts to loop through
 function minifyJS(jsFile) {
-  return (
-    src(jsFile.src)
-      .pipe(
-        plugins.plumber({
-          errorHandler(error) {
-            console.log(error.message);
-            this.emit('end');
-          }
-        })
-      )
-      .pipe(plugins.replace('$*cdn', packageJson.buildDirs[build].cdn))
-      .pipe(
-        webpack({
-          config: {
-            mode: 'production',
-            devtool: 'source-map',
-            output: { filename: jsFile.minName }
-          }
-        })
-      )
-      // .pipe(plugins.uglify()) // Mangle var names etc.
-      .pipe(plugins.plumber.stop())
-      .pipe(dest(paths.scripts.output))
-  ); // Spit out concat + minified file in ./build/
+  return src(jsFile.src)
+    .pipe(
+      plugins.plumber({
+        errorHandler(error) {
+          console.log(error.message);
+          this.emit('end');
+        }
+      })
+    )
+    .pipe(plugins.replace('$*cdn', packageJson.buildDirs[build].cdn))
+    .pipe(
+      webpack({
+        config: {
+          mode: buildMode, // detemined by buildMode const above
+          devtool: 'source-map', // Set a sourcemap for this build
+          output: { filename: jsFile.minName } // output name of the bundled js
+        }
+      })
+    )
+    .pipe(plugins.plumber.stop())
+    .pipe(dest(paths.scripts.output)); // Spit out concat + minified file in ./build/
 }
 
 // Minify, and concatenate scripts
