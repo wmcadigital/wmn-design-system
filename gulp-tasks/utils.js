595e7c37964401274cdecb8965ac3b7c68a5c22f
Dayle Salmon <dayle.salmon@wmca.org.uk>
2019-11-02 12:45:19 +0000

    removed redundant code

--- gulp-tasks/utils.js
+++ gulp-tasks/utils.js
@@ -1,23 +1,22 @@
 // Gulp requires
 const fs = require('fs');
-const { src, dest } = require('gulp');
 
 let build = 'local';
 // Function that is ran when buildAll is called to determine buildEnv
 // This matches the buildDirs in package.json
 switch (process.env.npm_config_build) {
-case 'staging':
-  build = 'staging';
-  break;
-case 'live':
-  build = 'live';
-  break;
-case 'netlify':
-  build = 'netlify';
-  break;
-default:
-  build = 'local';
-  break;
+  case 'staging':
+    build = 'staging';
+    break;
+  case 'live':
+    build = 'live';
+    break;
+  case 'netlify':
+    build = 'netlify';
+    break;
+  default:
+    build = 'local';
+    break;
 }
 
 module.exports.build = build;
