49d2e2d46025bf42f94c869f2990fa743c10d8a1
Dayle <dayle.salmon@wmca.org.uk>
2019-11-13 10:32:59 +0000

    Improved sprite sheet task

--- gulp-tasks/sprite-svgs.js
+++ gulp-tasks/sprite-svgs.js
@@ -2,12 +2,36 @@
 const { src, dest } = require('gulp');
 const plugins = require('gulp-load-plugins')();
 // Local requires
+const path = require('path');
 const paths = require('./paths.js');
 
 module.exports = () => {
   return src(paths.svgs.src)
     .pipe(plugins.rename({ prefix: 'wmnds-' }))
+    .pipe(
+      plugins.cheerio({
+        run: $ => {
+          $('[fill]').removeAttr('fill');
+        },
+        parserOptions: { xmlMode: true }
+      })
+    )
+    .pipe(
+      plugins.svgmin(file => {
+        const prefix = path.basename(file.relative, path.extname(file.relative));
+        return {
+          plugins: [
+            {
+              cleanupIDs: {
+                prefix: `${prefix}-`,
+                minify: true
+              }
+            }
+          ]
+        };
+      })
+    )
     .pipe(plugins.svgstore())
-    .pipe(plugins.rename({ basename: 'svg-sprite' }))
+    .pipe(plugins.rename({ basename: 'svg-sprite', extname: '.min.svg' }))
     .pipe(dest(paths.svgs.dest));
 };
