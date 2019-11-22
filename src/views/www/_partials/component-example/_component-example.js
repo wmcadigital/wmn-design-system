29cf32ba82f0a414d756375b3465c39bce9a4c6b
Dayle Salmon <dayle.salmon@wmca.org.uk>
2019-11-22 11:05:09 +0000

    Added show more/show less code button

--- src/views/www/_partials/component-example/_component-example.js
+++ src/views/www/_partials/component-example/_component-example.js
@@ -3,8 +3,19 @@ function showCode() {
   document.querySelectorAll('.wmnds-js-show-code').forEach(ele => {
     // when clicked
     ele.addEventListener('click', e => {
-      // expand code height to 100%
-      e.target.parentElement.querySelector('.hljs').style.maxHeight = '100%';
+      const btn = e.target || e.srcElement; // Set btn to the element from which the click came
+
+      // if the btn clicked contains this class, then it must be active, so reset it back to the norm
+      if (btn.classList.contains('wmnds-js-show-code--active')) {
+        btn.parentElement.querySelector('.hljs').style.maxHeight = '200px';
+        btn.classList.remove('wmnds-js-show-code--active');
+        btn.innerText = 'Show more code';
+      } else {
+        // Else expand the code preview to 100%
+        btn.parentElement.querySelector('.hljs').style.maxHeight = '100%';
+        btn.classList.add('wmnds-js-show-code--active');
+        btn.innerText = 'Show less code';
+      }
     });
   });
 }
@@ -14,13 +25,13 @@ export default () => {
   document.querySelectorAll('pre code').forEach(element => {
     // Run highlightJS for each pre code element found */
     hljs.highlightBlock(element);
-
+    // If the code preview is 192 height (without padding) then we need to display the 'show more code' button
     if (element.clientHeight >= 192) {
-      const htmlString = '<span class="wmnds-link wmnds-js-show-code">Show code</span>';
+      const htmlString = '<span class="wmnds-link wmnds-js-show-code">Show more code</span>';
 
       element.parentElement.insertAdjacentHTML('afterend', htmlString);
     }
-
-    showCode();
   });
-}
+
+  showCode(); // run show code function above when hljs has init
+};
