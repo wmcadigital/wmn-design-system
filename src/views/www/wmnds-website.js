0c320d61697d27799cd90dde1ba75a5590a6ff0e
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-01-14 14:33:14 +0000

    Removed old autocomplete js files

--- src/views/www/wmnds-website.js
+++ src/views/www/wmnds-website.js
@@ -1,5 +1,4 @@
 import colorPalettes from './pages/styles/colour-palettes/_color-palettes';
-import search from '../components/form-elements/search/_search';
 import highlightJS from './_partials/component-example/_component-example';
 
-window.addEventListener('DOMContentLoaded', (colorPalettes, search, highlightJS));
+window.addEventListener('DOMContentLoaded', (colorPalettes, highlightJS));
