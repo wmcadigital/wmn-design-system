546e03c130b39f94733aa79d5b51f3384c19e6f9
Houston <houston@Houstons-MacBook-Air.local>
2020-08-28 10:25:27 +0100

    keyboard arrow navigation

--- src/wmnds/patterns/header-v2/_header-v2.js
+++ src/wmnds/patterns/header-v2/_header-v2.js
@@ -1,10 +1,12 @@
 const header = () => {
+  // get mega menu elements
   const megaMenus = document.querySelectorAll('.wmnds-mega-menu');
+
   megaMenus.forEach(menu => {
     const topLevelLinks = menu.querySelectorAll('.wmnds-mega-menu__primary-menu-link');
-    topLevelLinks.forEach(topLevelLink => {
+    // handle events within each top level list item
+    topLevelLinks.forEach((topLevelLink, topLevelLinkIndex) => {
       let mousedown = false;
-      let menuIsActive = false;
       const topLevelListItem = topLevelLink.parentNode;
       const subMenuLinks = topLevelListItem.querySelectorAll('.wmnds-mega-menu__sub-menu-link');
 
@@ -28,34 +30,59 @@ const header = () => {
         }
       };
 
-      const handleKeydown = (e, key) => {
-        console.log(key);
-        // if key pressed is enter or space bar
-        if (key === 13 || key === 32) {
-          e.preventDefault();
-          // remove keyFocus to allow menu to show
-          setMenuActive();
-          // focus first menu item
-          console.log(subMenuLinks[0]);
-          subMenuLinks[0].focus();
-        } else if (key === 27) {
-          // if escape pressed
-          setMenuActive(false);
-        }
-      };
-
       const setMenuActive = active => {
         if (active !== false) {
           menu.classList.remove('keyFocus');
           topLevelListItem.classList.remove('keyFocus');
-          menuIsActive = true;
           menu.classList.add('active');
           topLevelListItem.classList.add('active');
         } else {
-          menuIsActive = false;
           menu.classList.remove('active');
           topLevelListItem.classList.remove('active');
           handleKeyFocus();
+        }
+      };
+
+      // currentIndex = index of the link that is currently focused
+      // array = array to move through
+      // direction = next, prev,
+      const getMenuLink = (currentIndex, array, direction) => {
+        // let link = null;
+        if (array) {
+          if (direction === 'prev') {
+            // return previous link in specified array if there is one else return null;
+            return array[currentIndex - 1] ? array[currentIndex - 1] : null;
+          } else if (direction === 'next') {
+            // return next link in specified array if there is one else return null;
+            return array[currentIndex + 1] ? array[currentIndex + 1] : null;
+          } else {
+            // return link with same index in specified array;
+            return array[currentIndex] ? array[currentIndex] : array[array.length - 1];
+          }
+        } else {
+          return null;
+        }
+      };
+
+      const handleKeydown = (e, key) => {
+        // if key pressed is enter, space bar or down arrow
+        if (key === 13 || key === 32 || key === 40) {
+          e.preventDefault();
+          // remove keyFocus to allow menu to show
+          setMenuActive();
+          // focus first menu item
+          subMenuLinks[0].focus();
+        } else if (key === 37) {
+          // left arrow
+          const prevLink = getMenuLink(topLevelLinkIndex, topLevelLinks, 'prev');
+          if (prevLink) prevLink.focus();
+        } else if (key === 39) {
+          // right arrow
+          const nextLink = getMenuLink(topLevelLinkIndex, topLevelLinks, 'next');
+          if (nextLink) nextLink.focus();
+        } else if (key === 27) {
+          // if escape pressed
+          setMenuActive(false);
         }
       };
 
@@ -63,6 +90,57 @@ const header = () => {
       topLevelLink.addEventListener('focus', handleKeyFocus);
       topLevelListItem.addEventListener('keydown', e => {
         handleKeydown(e, e.keyCode);
+      });
+
+      // array of mega menu links by column
+      const menuArray = [];
+      subMenuLinks.forEach((menuLink, menuIndex) => {
+        const thisList = menuLink.parentNode;
+        const thisListLinks = thisList.querySelectorAll('a');
+        // push list of links to array
+        menuArray.push(thisListLinks);
+
+        thisListLinks.forEach((link, linkIndex) => {
+          link.addEventListener('keydown', e => {
+            if (e.keyCode !== 27) {
+              e.stopPropagation();
+              if (e.keyCode === 39) {
+                // right arrow - go to link of same index in next menu list
+                e.preventDefault();
+                const nextMenuLink = getMenuLink(linkIndex, menuArray[menuIndex + 1]);
+                if (nextMenuLink) nextMenuLink.focus();
+              } else if (e.keyCode === 37) {
+                // left arrow - go to link of same index in previous menu list
+                e.preventDefault();
+                const prevMenuLink = getMenuLink(linkIndex, menuArray[menuIndex - 1]);
+                if (prevMenuLink) prevMenuLink.focus();
+              } else if (e.keyCode === 40) {
+                // down arrow - go to next link in current menu list
+                e.preventDefault();
+                // if next link doesnt exist try next menu first item else return null
+                const nextLink = getMenuLink(linkIndex, thisListLinks, 'next')
+                  ? getMenuLink(linkIndex, thisListLinks, 'next')
+                  : getMenuLink(-1, menuArray[menuIndex + 1], 'next');
+                if (nextLink) nextLink.focus();
+              } else if (e.keyCode === 38) {
+                // up arrow - go to previous item in current menu list
+                e.preventDefault();
+                const prevMenu = menuArray[menuIndex - 1];
+                let prevLink = null;
+                if (prevMenu || linkIndex > 0) {
+                  prevLink = getMenuLink(linkIndex, thisListLinks, 'prev')
+                    ? getMenuLink(linkIndex, thisListLinks, 'prev')
+                    : getMenuLink(prevMenu.length, prevMenu, 'prev');
+                }
+                if (prevLink) {
+                  prevLink.focus();
+                } else {
+                  setMenuActive(false);
+                }
+              }
+            }
+          });
+        });
       });
     });
   });
