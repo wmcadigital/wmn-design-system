83d57dd33d3c61f59b0f40cd5cb15546df6b4995
Houston <houston@Houstons-MacBook-Air.local>
2020-10-08 13:05:23 +0100

    added functions for compiling es6

--- src/www/wmnds-website.js
+++ src/www/wmnds-website.js
@@ -1,3 +1,4 @@
+import * as babel from '@babel/standalone';
 import * as Sentry from '@sentry/browser';
 import forEachPolyfill from './assets/vendor/js/polyfills/ie11-forEach';
 import colorPalettes from './pages/styles/colour-palettes/_color-palettes';
@@ -8,6 +9,21 @@ import footerJs from '../wmnds/patterns/footer/_footer';
 import accordionsJS from '../wmnds/components/accordion/_accordion';
 
 Sentry.init({ dsn: 'https://c72ff3aefb3e4a4585c61e5807411ae9@o378798.ingest.sentry.io/5285374' });
+
+const jsScripts = [headerJs, footerJs, cookies];
+
+const constructJsExample = (func, target) => {
+  target.innerHTML = babel.transform(func, { presets: ['env'] }).code;
+};
+
+jsScripts.forEach(script => {
+  const codeExamples = document.querySelectorAll('code.js');
+  codeExamples.forEach(codeExample => {
+    if (codeExample.dataset.content === script.name) {
+      constructJsExample(script, codeExample);
+    }
+  });
+});
 
 const icons = () => {
   // Ajax SVG in, SVGS are referenced in app (Icon component)
