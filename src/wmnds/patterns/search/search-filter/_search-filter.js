73ae6ecae49b6b2d0e710c649198ca0bdb2fbee7
Houston <houston@Houstons-MacBook-Air.local>
2020-10-16 12:03:44 +0100

    added filter example js

--- src/wmnds/patterns/search/search-filter/_search-filter.js
+++ src/wmnds/patterns/search/search-filter/_search-filter.js
@@ -3,51 +3,54 @@ const searchFilterJs = () => {
   const hideBtn = document.getElementById('hide_filter_btn');
   const showResults = document.getElementById('show_results_btn');
   const searchFilter = document.getElementById('search_filter');
-  const filterOptions = [...searchFilter.querySelectorAll('.wmnds-fe-checkboxes__input')];
 
-  filterOptions.forEach(option => {
-    option.addEventListener('change', () => {
-      if (filterOptions.some(input => input.checked)) {
-        searchFilter.classList.add('wmnds-search-filter--has-inputs-checked');
-      } else {
-        searchFilter.classList.remove('wmnds-search-filter--has-inputs-checked');
-      }
-    });
-  });
+  if (searchFilter) {
+    const filterOptions = searchFilter.querySelectorAll('.wmnds-fe-checkboxes__input');
 
-  const showFilter = show => {
-    if (show === false) {
-      searchFilter.classList.remove('wmnds-search-filter--is-open');
-    } else {
-      searchFilter.classList.add('wmnds-search-filter--is-open');
-    }
-  };
-
-  const clearFilters = () => {
     filterOptions.forEach(option => {
-      option.checked = false; // eslint-disable-line no-param-reassign
+      option.addEventListener('change', () => {
+        if ([...filterOptions].some(input => input.checked)) {
+          searchFilter.classList.add('wmnds-search-filter--has-inputs-checked');
+        } else {
+          searchFilter.classList.remove('wmnds-search-filter--has-inputs-checked');
+        }
+      });
     });
-    searchFilter.classList.remove('wmnds-search-filter--has-inputs-checked');
-  };
 
-  showBtn.addEventListener('click', e => {
-    e.preventDefault();
-    showFilter(true);
-  });
+    const showFilter = show => {
+      if (show === false) {
+        searchFilter.classList.remove('wmnds-search-filter--is-open');
+      } else {
+        searchFilter.classList.add('wmnds-search-filter--is-open');
+      }
+    };
 
-  hideBtn.addEventListener('click', e => {
-    e.preventDefault();
-    showFilter(false);
-  });
+    const clearFilters = () => {
+      filterOptions.forEach(option => {
+        option.checked = false; // eslint-disable-line no-param-reassign
+      });
+      searchFilter.classList.remove('wmnds-search-filter--has-inputs-checked');
+    };
 
-  document.querySelectorAll('.wmnds-search-filter__clear-all').forEach(clearBtn => {
-    clearBtn.addEventListener('click', e => {
+    showBtn.addEventListener('click', e => {
       e.preventDefault();
-      clearFilters();
+      showFilter(true);
     });
-  });
 
-  showResults.addEventListener('click', () => showFilter(false));
+    hideBtn.addEventListener('click', e => {
+      e.preventDefault();
+      showFilter(false);
+    });
+
+    document.querySelectorAll('.wmnds-search-filter__clear-all').forEach(clearBtn => {
+      clearBtn.addEventListener('click', e => {
+        e.preventDefault();
+        clearFilters();
+      });
+    });
+
+    showResults.addEventListener('click', () => showFilter(false));
+  }
 };
 
 export default searchFilterJs;
