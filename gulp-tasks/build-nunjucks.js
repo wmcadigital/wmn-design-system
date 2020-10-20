cef2ed4b551b60ad2f597c90e297582b6019decb
Gil <43111519+gldgrnt@users.noreply.github.com>
2020-10-20 13:00:02 +0100

    Add A to Z style guide (#450)
    * Adds style guide entries
    
    We will add any future entries to this JSON file.
    
    * Changes <h4> to <h6> as accordions are H4, entry title will be H5.
    
    * Creates style guide page
    
    * Update style-guide-entries.json
    
    Adds letter property, prettifies code.
    
    * Changes page name for consistency compared to other pages
    
    * Update index.njk
    
    Prettier
    
    * Adds entries for WMCA and TfWM
    
    * Add a to z style guide to navigation
    
    * Update a to z page
    
    * Add js for a to z page
    
    * Rename a to z json file
    
    * Add dynamic importing of json data files
    
    * Add bottom margin to each accordion
    
    * Fix typos in comments
    
    Co-authored-by: mrmjprice <05pricem@gmail.com>
    Co-authored-by: Houston Blyden <h.blyden@gmail.com>
    Co-authored-by: Catia Costa <34938764+catiarodriguescosta@users.noreply.github.com>

--- gulp-tasks/build-nunjucks.js
+++ gulp-tasks/build-nunjucks.js
@@ -1,14 +1,25 @@
 // Gulp requires
+const fs = require('fs');
+const path = require('path');
 const { src, dest } = require('gulp');
 const plugins = require('gulp-load-plugins')();
 // Local requires
 const paths = require('./paths.js');
 const { packageJson, build } = require('./utils');
+// Data requires
 const njkData = require('../src/www/data.njk.json');
+
+// Automatically parse json from data files
+// Data files must have the same name as their parent directory and have the extension .njk.json
+const getNjkData = ({ path: filePath }) => {
+  const dataFilePath = `${path.dirname(filePath)}/${path.dirname(filePath).split('/').slice(-1)[0]}.njk.json`;
+  return fs.existsSync(dataFilePath) && JSON.parse(fs.readFileSync(dataFilePath));
+};
 
 module.exports = () => {
   return src(paths.nunjucks.websiteSrc)
     .pipe(plugins.data(() => njkData))
+    .pipe(plugins.data(file => getNjkData(file)))
     .pipe(
       plugins.nunjucksRender({
         path: 'src/',
