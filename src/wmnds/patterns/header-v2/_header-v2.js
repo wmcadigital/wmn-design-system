347cd225bf68a73f5fad5a4db358390789902284
Houston <houston@Houstons-MacBook-Air.local>
2020-10-27 14:26:30 +0000

    Fixed header bugs

--- src/wmnds/patterns/header-v2/_header-v2.js
+++ src/wmnds/patterns/header-v2/_header-v2.js
@@ -4,18 +4,17 @@ const headerJs = () => {
 
   const mobileMenu = window.matchMedia('(max-width: 767px)');
 
+  // get search list items
   fetch('/ga.json')
     .then(response => response.json())
     .then(data => {
-      console.log(data.rows);
       data.rows.forEach(row => {
         const linkItem = document.createElement('LI');
         linkItem.setAttribute('class', 'wmnds-search-list__item');
-        linkItem.innerHTML = `<a href="${row[0]}" class="wmnds-link wmnds-link--with-chevron">${row[1]} <svg class="wmnds-link__chevron wmnds-link__chevron--right">
-          <use xlink:href="#wmnds-general-chevron-right" href="#wmnds-general-chevron-right"></use>
-        </svg></a>`;
+        linkItem.innerHTML = `<a href="${row[0]}" class="wmnds-link wmnds-link--with-chevron">${row[1]} <svg class="wmnds-link__chevron wmnds-link__chevron--right"><use xlink:href="#wmnds-general-chevron-right" href="#wmnds-general-chevron-right"></use></svg></a>`;
         document.querySelectorAll('.wmnds-search-list').forEach(searchList => {
-          searchList.appendChild(linkItem);
+          const el = linkItem.cloneNode(true);
+          searchList.appendChild(el);
         });
       });
     });
@@ -29,8 +28,8 @@ const headerJs = () => {
 
         const topLevelMenuBtn = menu.querySelectorAll('.wmnds-mega-menu__link-arrow-icon-btn');
         const searchBtn = menu.querySelector('.wmnds-mega-menu__search-btn');
-        const mobileMenuIsOpen = { menu: false, primary: false, search: false };
         // object to see which menu/menu level is open
+        const mobileMenuIsOpen = { menu: false, primary: false, search: false };
 
         // handle mobile menu toggle
         mobileToggle.addEventListener('click', () => {
@@ -100,6 +99,10 @@ const headerJs = () => {
     mobileMenu.addListener(handleMobileMenu);
 
     const topLevelLinks = menu.querySelectorAll('.wmnds-mega-menu__primary-menu-link');
+    let menuDelay = false;
+    let enterTimeOut;
+    let leaveTimeOut;
+    const delayTime = 300;
 
     // handle events within each top level list item
     topLevelLinks.forEach((topLevelLink, topLevelLinkIndex) => {
@@ -129,8 +132,8 @@ const headerJs = () => {
       };
 
       // handle setting the active class on menu and list items
-      const setMenuActive = (active, focusLink) => {
-        if (active !== false) {
+      const setMenuActive = (active = true, focusLink) => {
+        if (active) {
           menu.classList.add('active');
           clearActiveListItems();
           // add active class to current item
@@ -166,28 +169,33 @@ const headerJs = () => {
         return menuLink;
       };
 
+      const openSubMenu = e => {
+        // check if list item has a mega menu
+        if (topLevelListItem.querySelectorAll('.wmnds-mega-menu__container').length) {
+          e.preventDefault();
+          // remove keyFocus to allow menu to show
+          setMenuActive(true);
+          // focus first menu item
+          if (topLevelListItem.contains(subMenuLinks[0])) {
+            subMenuLinks[0].focus();
+          } else if (topLevelListItem.querySelector('.wmnds-search-bar__input')) {
+            topLevelListItem.querySelector('.wmnds-search-bar__input').focus();
+          }
+        }
+      };
+
       const handleKeydown = (e, key) => {
         e.stopPropagation();
         // if key pressed is enter, space bar or down arrow
         if (key === 13 || key === 32 || key === 40) {
-          // check if list item has a mega menu
-          const openSubMenu = () => {
-            if (topLevelListItem.querySelectorAll('.wmnds-mega-menu__container').length) {
-              e.preventDefault();
-              // remove keyFocus to allow menu to show
-              setMenuActive(true);
-              // focus first menu item
-              subMenuLinks[0].focus();
-            }
-          };
           // enter
           // check if link exists
           if (key === 13) {
             if (!topLevelLink.tagName === 'a' || !topLevelLink.getAttribute('href')) {
-              openSubMenu();
+              openSubMenu(e);
             }
           } else {
-            openSubMenu();
+            openSubMenu(e);
           }
         } else if (key === 37) {
           // left arrow
@@ -209,20 +217,46 @@ const headerJs = () => {
 
       if (isTopLevelWithMenu) {
         topLevelLink.addEventListener('mouseover', () => {
-          setMenuActive();
+          if (!menuDelay) {
+            // if no menuDelay is active just open the menu
+            setMenuActive();
+          } else {
+            // if menuDelay is active, clear all timeouts and start a new one
+            clearTimeout(enterTimeOut);
+            clearTimeout(leaveTimeOut);
+
+            enterTimeOut = setTimeout(() => {
+              // enter timeout completed, open menu and kill delay
+              menuDelay = false;
+              setMenuActive();
+            }, delayTime);
+          }
+        });
+        topLevelListItem.querySelector('.wmnds-mega-menu__container').addEventListener('mouseover', () => {
+          if (menuDelay) {
+            // if container is rehovered before timeout is done, clear all timeouts kill the delay
+            clearTimeout(enterTimeOut);
+            clearTimeout(leaveTimeOut);
+            menuDelay = false;
+          }
         });
         topLevelListItem.addEventListener('mouseleave', () => {
-          setMenuActive(false, false);
-          clearActiveListItems();
+          menuDelay = true;
+          // leave timeout is active
+          leaveTimeOut = setTimeout(() => {
+            // leave timeout completed, close menu
+            setMenuActive(false, false);
+            menuDelay = false;
+          }, delayTime);
         });
 
-        // array of mega menu links by column
         topLevelListItem.addEventListener('blur', setMenuActive(false, false));
       }
 
       topLevelListItem.addEventListener('keydown', e => {
         handleKeydown(e, e.keyCode);
       });
+
       // top lvl link event listeners
       topLevelLink.addEventListener('mousedown', e => {
         // prevent link focus on click
@@ -252,8 +286,8 @@ const headerJs = () => {
                 e.preventDefault();
                 const prevMenuLink = getMenuLink(linkIndex, menuArray[menuIndex - 1]);
                 if (prevMenuLink) prevMenuLink.focus();
-              } else if (e.keyCode === 40) {
-                // down arrow - go to next link in current menu list
+              } else if (e.keyCode === 40 || (e.keyCode === 9 && !e.shiftKey)) {
+                // down arrow or tab - go to next link in current menu list
                 e.preventDefault();
                 // if next link doesn't exist try next menu first item else return null
                 const nextLink = getMenuLink(linkIndex, thisListLinks, 'next')
@@ -267,8 +301,8 @@ const headerJs = () => {
                     getMenuLink(topLevelLinkIndex, topLevelLinks, 'next').focus();
                   }
                 }
-              } else if (e.keyCode === 38) {
-                // up arrow - go to previous item in current menu list
+              } else if (e.keyCode === 38 || (e.shiftKey && e.keyCode === 9)) {
+                // up arrow or shift + tab - go to previous item in current menu list
                 e.preventDefault();
                 const prevMenu = menuArray[menuIndex - 1];
                 let prevLink = null;
