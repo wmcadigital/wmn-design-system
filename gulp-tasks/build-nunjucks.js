66280b8820b8c9762ad64c1dfe7bc493ef202159
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-12-22 15:33:14 +0000

    Added dynamic release date on build

--- gulp-tasks/build-nunjucks.js
+++ gulp-tasks/build-nunjucks.js
@@ -106,6 +106,18 @@ const buildingTemplates = () => {
       .pipe(plugins.rename({ extname: '.html' }))
       .pipe(plugins.replace('$*cdn', packageJson.buildDirs[build].cdn))
       .pipe(plugins.replace('$*version', versionNumber))
+      .pipe(
+        plugins.replace('$*releaseDate', () => {
+          // Show todays date in place of the release date
+          // When the site builds from release this will stay static until the next release
+          const d = new Date();
+          const ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d);
+          const mo = new Intl.DateTimeFormat('en', { month: 'long' }).format(d);
+          const da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
+
+          return `${da} ${mo} ${ye}`;
+        })
+      )
       .pipe(plugins.formatHtml())
       .pipe(plugins.htmlmin({ removeComments: true, collapseWhitespace: true }))
       .pipe(dest(paths.nunjucks.output))
