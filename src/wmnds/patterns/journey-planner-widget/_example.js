a08287bb67c3bb753bca53eeaf10d3075ce24f6a
Catia Costa <catia.rodrigues.costa@gmail.com>
2021-05-05 12:31:55 +0100

    Update jpw example and properties.

--- src/wmnds/patterns/journey-planner-widget/_example.js
+++ src/wmnds/patterns/journey-planner-widget/_example.js
@@ -1,21 +1,5 @@
-const accordionsJS = () => {
-  const accordions = document.querySelectorAll('.wmnds-accordion');
+const journeyPlannerWidgetJS = () => {
 
-  accordions.forEach(accordion => {
-    const accordionBtn = accordion.querySelector('.wmnds-accordion__summary-wrapper');
-
-    const toggleAccordion = () => {
-      if (accordion.classList.contains('wmnds-is--open')) {
-        accordion.classList.remove('wmnds-is--open');
-        accordionBtn.setAttribute('aria-expanded', false);
-      } else {
-        accordion.classList.add('wmnds-is--open');
-        accordionBtn.setAttribute('aria-expanded', true);
-      }
-    };
-
-    accordionBtn.addEventListener('click', toggleAccordion);
-  });
 };
 
-export default accordionsJS;
+export default journeyPlannerWidgetJS;
