6f31f43cb3c8bc621beb65e5e5df97efecafa721
Catia Costa <catia.rodrigues.costa@gmail.com>
2021-05-18 14:21:45 +0100

    Form submission is now working properly. JS is updated and html are updated.

--- src/wmnds/patterns/find-a-timetable-widget/_example.js
+++ src/wmnds/patterns/find-a-timetable-widget/_example.js
@@ -4,10 +4,20 @@ const findATimetableJS = () => {
     e.target.closest('.wmnds-find-a-timetable-widget').classList.toggle('wmnds-is--open');
   };
 
+  const onSubmitForm = e => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const queryString = new URLSearchParams(data).toString();
+    window.location.href = `https://www.wmnetwork.co.uk/plan-your-journey/timetables/#/?${queryString}`;
+  };
+
   findATimetableComponents.forEach(findATimetableComponent => {
     findATimetableComponent
       .querySelector('div.wmnds-btn.wmnds-btn--mode')
       .addEventListener('click', toggleOpenClass);
+    findATimetableComponent
+      .querySelector('.wmnds-find-a-timetable-widget__form')
+      .addEventListener('submit', onSubmitForm);
   });
 };
 
