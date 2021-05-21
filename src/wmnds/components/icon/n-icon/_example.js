3d30369d9f024849361fbc84f311002536cfc0d4
Dayle Salmon <daylesalmon@gmail.com>
2021-05-21 14:45:08 +0100

    n-icons is almost working with plugin

--- src/wmnds/components/icon/n-icon/_example.js
+++ src/wmnds/components/icon/n-icon/_example.js
@@ -2,27 +2,27 @@ import * as findAndReplaceDOMText from 'findandreplacedomtext';
 
 const nIcons = () => {
   // Function to replace text with our icon
-  const replaceTextWithIcon = textNode => {
-    const modifiedTextNode = textNode;
-    if (modifiedTextNode.innerText.charAt(0) !== 'n') return textNode; // If textNode doesn't include our matched text, then return
+  // const replaceTextWithIcon = textNode => {
+  //   const modifiedTextNode = textNode;
+  //   if (modifiedTextNode.innerText.charAt(0) !== 'n') return textNode; // If textNode doesn't include our matched text, then return
 
-    // Otherwise...
-    const textWithoutN = modifiedTextNode.innerText.substring(1); // Chop the first character from our string (the 'n') as it will be replaced with an icon
-    console.log({ textWithoutN });
-    // HTML/SVG of icon we will use
-    const svgIcon = `
-    <svg class="wmnds-n-icon__svg">
-      <title>N-Network icon</title>
-      <desc>A hexagon with the letter 'n' inside of it.</desc>
-      <use xlink:href="#wmnds-general-n-ticket" href="#wmnds-general-n-ticket"></use>
-    </svg>`;
-    // Then replace our found text with 'n' icon and text without 'n'
-    modifiedTextNode.innerHTML = `${svgIcon}${textWithoutN}`;
+  //   // Otherwise...
+  //   const textWithoutN = modifiedTextNode.innerText.substring(1); // Chop the first character from our string (the 'n') as it will be replaced with an icon
+  //   console.log({ textWithoutN });
+  //   // HTML/SVG of icon we will use
+  //   const svgIcon = `
+  //   <svg class="wmnds-n-icon__svg">
+  //     <title>N-Network icon</title>
+  //     <desc>A hexagon with the letter 'n' inside of it.</desc>
+  //     <use xlink:href="#wmnds-general-n-ticket" href="#wmnds-general-n-ticket"></use>
+  //   </svg>`;
+  //   // Then replace our found text with 'n' icon and text without 'n'
+  //   modifiedTextNode.innerHTML = `${svgIcon}${textWithoutN}`;
 
-    console.log({ modifiedTextNode });
+  //   console.log({ modifiedTextNode });
 
-    return modifiedTextNode; // Return modified textNode back
-  };
+  //   return modifiedTextNode; // Return modified textNode back
+  // };
 
   // function replacePhraseInHTML(htmlEle, phrase) {
   //   const phraseToReplace = phrase;
@@ -45,24 +45,42 @@ const nIcons = () => {
   // const elementsToCheck = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 
   const t0 = performance.now();
+
+  const convertStringToNode = string => {
+    const textWithoutN = string.substring(1); // Chop the first character from our string (the 'n') as it will be replaced with an icon
+    // HTML/SVG of icon we will use
+    const svgIcon = `
+      <svg class="wmnds-n-icon__svg">
+        <title>N-Network icon</title>
+        <desc>A hexagon with the letter 'n' inside of it.</desc>
+        <use xlink:href="#wmnds-general-n-ticket" href="#wmnds-general-n-ticket"></use>
+      </svg>`;
+    // Then replace our found text with 'n' icon and text without 'n'
+    const html = `<span class="wmnds-n-icon">${svgIcon}${textWithoutN}</span>`;
+    return document.createRange().createContextualFragment(html).firstChild;
+  };
+
   const replaceAll = () =>
     findAndReplaceDOMText(document.querySelector('body'), {
       preset: 'prose',
       find: /(nNetwork|nTicket|nTrain|nBus)/gi,
-      wrap: 'span',
-      wrapClass: 'wmnds-n-icon'
+      // wrap: 'span',
+      // wrapClass: 'wmnds-n-icon',
+      replace: (portion, match) => {
+        return convertStringToNode(match[0]);
+      }
     });
 
   replaceAll();
 
-  const nIconNodes = document.querySelectorAll('.wmnds-n-icon');
+  // const nIconNodes = document.querySelectorAll('.wmnds-n-icon');
 
-  nIconNodes.forEach(nIconNode => {
-    let newNode = nIconNode;
-    newNode = replaceTextWithIcon(nIconNode);
+  // nIconNodes.forEach(nIconNode => {
+  //   let newNode = nIconNode;
+  //   newNode = replaceTextWithIcon(nIconNode);
 
-    return newNode;
-  });
+  //   return newNode;
+  // });
   // const elementsToCheck = document.querySelectorAll('.wmnds-n-icon');
   // Loop through each phrase and update the DOM body innerHTML with the replaced phrase
   // elementsToCheck.forEach(element => {
