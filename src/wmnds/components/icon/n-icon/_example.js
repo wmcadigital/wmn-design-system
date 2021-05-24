de6efeaf06139640e65910c7e330326d0d88ca1f
Dayle Salmon <daylesalmon@gmail.com>
2021-05-24 12:19:24 +0100

    Updated JS logic with better comments

--- src/wmnds/components/icon/n-icon/_example.js
+++ src/wmnds/components/icon/n-icon/_example.js
@@ -1,6 +1,10 @@
-import * as findAndReplaceDOMText from 'findandreplacedomtext';
+import * as findAndReplaceDOMText from 'findandreplacedomtext'; // https://github.com/padolsey/findAndReplaceDOMText
 
 const nIcons = () => {
+  // Convert a html string into an actual HTML node
+  const convertHtmlStringToNode = htmlString =>
+    document.createRange().createContextualFragment(htmlString).firstChild;
+
   const convertStringToNode = string => {
     const textWithoutN = string.substring(1); // Chop the first character from our string (the 'n') as it will be replaced with an icon
     // HTML/SVG of icon we will use
@@ -11,22 +15,25 @@ const nIcons = () => {
         <use xlink:href="#wmnds-general-n-ticket" href="#wmnds-general-n-ticket"></use>
       </svg>`;
     // Then replace our found text with 'n' icon and text without 'n'
-    const html = `<span class="wmnds-n-icon">${svgIcon}${textWithoutN}</span>`;
-    return document.createRange().createContextualFragment(html).firstChild;
+    const htmlString = `<span class="wmnds-n-icon">${svgIcon}${textWithoutN}</span>`;
+
+    return convertHtmlStringToNode(htmlString);
   };
 
-  const replaceAll = () => {
-    const body = document.querySelector('body');
-    findAndReplaceDOMText(body, {
+  const replaceNText = () => {
+    const wordsToReplace = /(nNetwork|nTicket|nTrain|nBus)/; // Pipe in any words that we should replace the 'n' with the SVG 'n' icon (not case sensitive)
+    const bodyEle = document.querySelector('body');
+
+    // Read https://github.com/padolsey/findAndReplaceDOMText on how this works
+    findAndReplaceDOMText(bodyEle, {
       preset: 'prose',
-      find: /(nNetwork|nTicket|nTrain|nBus)/gi,
-      replace: (portion, match) => {
-        return convertStringToNode(match[0]);
-      }
+      find: new RegExp(wordsToReplace, 'gi'),
+      replace: (_portion, match) => convertStringToNode(match[0])
     });
   };
 
-  replaceAll();
+  // START HERE
+  replaceNText();
 };
 
 export default nIcons;
